4d2b169ad6caf8f1fd8c92ad4e9d85d24c282735
keshav-potomac <51111784+keshav-potomac@users.noreply.github.com>
2020-11-12 21:31:19 +0530

    code persists added.

--- src/redux/root.reducers.js
+++ src/redux/root.reducers.js
@@ -1,9 +1,16 @@
 import { combineReducers } from 'redux';
 import  CartReducers from '../redux/cart/cart.reducers';
 import { UserReducers } from '../redux/user/user.reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const configPersist = {
+ key: 'root',
+ storage,
+ whitelist: ['cart']
+};
 const rootReducer = combineReducers({
     cart: CartReducers,
     user:UserReducers
   });
   
-  export default rootReducer;
+  export default persistReducer(configPersist,rootReducer);
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 //Root Reducers...
 import rootReducer from './../root.reducers';
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'development') {
    middlewares.push(logger);
 }
 // Reducers Exist Store 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+ export const persistor = persistStore(store);
 
-
-export default { store };
+export default { store ,persistor };
